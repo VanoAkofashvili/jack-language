9ef805c35d377231e35e7492a29cdbcd0b973e5c
Vaniko Akopashvili <vanikoakofa@gmail.com>
2025-06-07 21:39:05 +0400

    Fix CompilationEngine type handling

--- syntax-analyzer/CompilationEngine.ts
+++ syntax-analyzer/CompilationEngine.ts
@@ -13,8 +13,15 @@ export class CompilationEngine {
     console.log(this.output)
   }
 
+  static Types = ['int', 'char', 'boolean']
+  static Void = 'void'
+
+  private currentToken() {
+    return this.tokenizer.getCurrentToken()
+  }
+
   private eat(expect?: string) {
-    const token = this.tokenizer.getCurrentToken()
+    const token = this.currentToken()
 
     if (expect && (expect !== token.value)) {
       throw new InvalidTokenError(token.value)
@@ -27,24 +34,31 @@ export class CompilationEngine {
   }
 
   private eatIdentifier() {
-    const token = this.tokenizer.getCurrentToken()
+    const token = this.currentToken()
     if (token.type !== 'IDENTIFIER') {
       throw new ExpectedIdentifierError(token.type)
     }
     this.eat()
   }
 
-  public eatType() {
-    const token = this.tokenizer.getCurrentToken()
+  public eatType(expansionList?: string[], config?: { allowAnyType: boolean }) {
+    const token = this.currentToken()
 
-    if (token.type !== 'KEYWORD') throw new InvalidTokenError(token.value)
+    console.log({token})
+    if (token.type === 'IDENTIFIER' || token.type === 'KEYWORD') {
+      const typesToCheck = CompilationEngine.Types.concat(expansionList)
 
-    if (['int', 'char', 'boolean'].includes(token.value)) {
-      this.tag(TokenTypeMapping.KEYWORD, token.value)
-      this.tokenizer.advance()
+      if (config?.allowAnyType || typesToCheck.includes(token.value)) {
+        this.tag(TokenTypeMapping[token.type], token.value)
+        this.tokenizer.advance()
+      } else {
+        throw new InvalidTypeError(token.value)
+      }
     } else {
-      throw new InvalidTypeError(token.value)
+      throw new InvalidTokenError(token.value)
     }
+
+
   }
 
   private startTag(tag: string) {
@@ -68,14 +82,14 @@ export class CompilationEngine {
     this.eat('{')
 
     this.compileClassVarDec() // zero or one
-    // this.compileSubroutine() // zero or one
+    this.compileSubroutine() // zero or one
 
     // this.eat('}')
     this.endTag('class')
 
   }
   compileClassVarDec() {
-    const currentToken = this.tokenizer.getCurrentToken()
+    const currentToken =this.currentToken()
 
 
     if (!(['static', 'field'].includes(currentToken.value))) return
@@ -86,14 +100,34 @@ export class CompilationEngine {
     this.eatType() // int | boolean | ...
     this.eat() // varName
 
-    while(this.tokenizer.getCurrentToken().value === ',') {
+    while(this.currentToken().value === ',') {
       this.eat(',')
       this.eatIdentifier()
     }
     this.eat(';')
     this.endTag('classVarDec')
   }
-  compileSubroutine() {}
+  compileSubroutine() {
+    if (!(['constructor', 'function', 'method'].includes(this.currentToken().value))) return
+    this.eat() // keyword - method type
+
+
+    // eat common types + void
+    this.eatType([CompilationEngine.Void], {allowAnyType: true})
+    this.eatIdentifier()
+    this.eat('(')
+    // //TODO: handle parameter list
+    // this.compileParameterList() // zero or more
+    this.eat(')')
+    this.eat('{')
+
+    // TODO
+    // this.compileSubroutineBody()
+
+    // this.eat('}')
+
+
+  }
   compileParameterList() {}
   compileSubroutineBody() {}
   compileVarDec() {}
